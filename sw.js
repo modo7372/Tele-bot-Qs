db163004a630ec11b8c140dbe5d222f649514c55
modo7372 <meedoasadel@gmail.com>
2026-01-16 18:59:20 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'medquiz-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/config.js',
+  './js/ui.js',
+  './js/data.js',
+  './js/game.js',
+  './js/main.js',
+  './questions_list.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
