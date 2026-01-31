9c7016b872ab3df5fba55cac5cac8313a15b5d51
modo7372 <meedoasadel@gmail.com>
2026-01-31 18:53:47 +0200

    Update cache version to medquiz-v2.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'medquiz-v2.3'; // هام: قم بتغيير هذا الرقم عند كل تحديث
+const CACHE_NAME = 'medquiz-v2.4'; // هام: قم بتغيير هذا الرقم عند كل تحديث
 const ASSETS = [
   './',
   './index.html',
