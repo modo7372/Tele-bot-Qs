c4923a4dc0d35908298c9198903693eef97b4e10
modo7372 <meedoasadel@gmail.com>
2026-02-11 22:01:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,22 @@
-const CACHE_NAME = 'medquiz-v10.9'; // هام: قم بتغيير هذا الرقم عند كل تحديث
+const CACHE_NAME = 'medquiz-v11';
+
 const ASSETS = [
   './',
   './index.html',
   './css/style.css',
-  './js/config.js',
-  './js/ui.js',
   './js/data.js',
+  './js/ui.js',
   './js/game.js',
-  './js/main.js',
   './questions_list.json'
 ];
 
 self.addEventListener('install', e => {
-  // إجبار الـ Service Worker الجديد على التفعيل فوراً
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
-  // السيطرة الفورية على الصفحات المفتوحة
   e.waitUntil(clients.claim());
-  
-  // حذف الكاش القديم
   e.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.map(key => {
